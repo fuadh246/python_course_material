e689a56aca0889820960d9cf7d38fb4b7fea6c1b
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-08 00:44:18 -0400

    file added

--- file_handling/day_19_file.py
+++ file_handling/day_19_file.py
@@ -1,3 +1,4 @@
+# read the whole filw
 file = open('file_handling/reading_file_example.txt')
 text = file.read()
 print(type(text))
@@ -5,6 +6,7 @@ print(text)
 file.close()
 print()
 
+# read a single line
 file = open('file_handling/reading_file_example.txt')
 line = file.readline()
 print(type(line))
@@ -12,12 +14,15 @@ print(line)
 file.close()
 print()
 
+# read lines as a list
 file = open('file_handling/reading_file_example.txt')
 lines = file.readlines()
 print(type(lines))
 print(lines)
 file.close()
 print()
+
+
 
 
 
