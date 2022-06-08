910afbe3a5abd29dcfb5cd3679bd749f03633306
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-08 18:19:31 -0400

    workring

--- mypacakge/greet.py
+++ mypacakge/greet.py
@@ -1,4 +1,10 @@
 # mypackage/greet.py
 # greet.py
 def greet_person(firstname, lastname):
-    return f'{firstname} {lastname}, welcome to 30DaysOfPython Challenge!'
+    return f'{firstname} {lastname}, welcome!'
+
+def say_hello():
+    fullname = greet_person('Fuad', 'Hassan')
+    print(f'{fullname} \nhello')
+
+say_hello()
