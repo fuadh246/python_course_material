fc7d113891190e597139a0d3f93e670bb3c12c7a
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-08 01:07:49 -0400

    file added

--- file_handling/day_19_file.py
+++ file_handling/day_19_file.py
@@ -22,7 +22,33 @@ print(lines)
 file.close()
 print()
 
+# read lines as a list using splitlines
+file = open('file_handling/reading_file_example.txt')
+lines = file.read().splitlines()
+print(type(lines))
+print(lines)
+file.close()
+print()
 
 
+with open('file_handling/reading_file_example.txt') as file:
+    if file.readable:
+        lines = file.read().upper().splitlines()
+        print(type(lines))
+        print(lines)
 
+# Opening Files for Writing and Updating
+with open('file_handling/reading_file_example.txt','a') as file:
+    #file.write('This text has to be appended at the end\n')
+    pass
 
+with open('file_handling/writing_file_example.txt','w') as file:
+    pass
+    file.write('This text will be written in a newly created file\n')
+
+# removing files
+import os 
+if os.path.exists('file_handling/writing_file_example.txt'):
+    os.remove('file_handling/writing_file_example.txt')
+else:
+    print('file does not exist')
