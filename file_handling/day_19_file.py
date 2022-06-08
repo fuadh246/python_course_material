19eafaadc71ffd25fc1af3051c54923d7045dad5
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-06-08 00:43:45 -0400

    file added

--- file_handling/day_19_file.py
+++ file_handling/day_19_file.py
@@ -1,2 +1,23 @@
-f = open('file_handling/reading_file_example.txt')
-print(f)
+file = open('file_handling/reading_file_example.txt')
+text = file.read()
+print(type(text))
+print(text)
+file.close()
+print()
+
+file = open('file_handling/reading_file_example.txt')
+line = file.readline()
+print(type(line))
+print(line)
+file.close()
+print()
+
+file = open('file_handling/reading_file_example.txt')
+lines = file.readlines()
+print(type(lines))
+print(lines)
+file.close()
+print()
+
+
+
